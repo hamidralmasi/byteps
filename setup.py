d06d7fec8db7112dfe2dcfa24285e76a4b076c75
halmas3 <halmas3@localhost>
2022-02-09 19:59:23 -0600

    changing to -std=c++14

--- setup.py
+++ setup.py
@@ -176,7 +176,7 @@ def get_mpi_flags():
 
 def get_cpp_flags(build_ext):
     last_err = None
-    default_flags = ['-std=c++11', '-fPIC', '-Ofast', '-Wall', '-fopenmp', '-march=native', '-mno-avx512f']
+    default_flags = ['-std=c++14', '-fPIC', '-Ofast', '-Wall', '-fopenmp', '-march=native', '-mno-avx512f']
     flags_to_try = []
     if sys.platform == 'darwin':
         # Darwin most likely will have Clang, which has libc++.
